5d909a932250b3b1d5ca38c84fba0f84c6a0797c
Kiwi <agiannoulis@mavestro.co>
2021-09-16 03:30:33 +0300

    add more fields

--- module/helpers.go
+++ module/helpers.go
@@ -3,23 +3,25 @@ package module
 import (
 	"github.com/ProjectAthenaa/sonic-core/protos/module"
 	"github.com/ProjectAthenaa/sonic-core/sonic/frame"
+	"github.com/ProjectAthenaa/supreme/config"
+	"strings"
 )
 
-func (tk *Task) FormatPhone() string{
-	return tk.Data.Profile.Shipping.PhoneNumber[:3] +"+"+ tk.Data.Profile.Shipping.PhoneNumber[3:6] +"+"+ tk.Data.Profile.Shipping.PhoneNumber[6:]
+func (tk *Task) FormatPhone() string {
+	return tk.Data.Profile.Shipping.PhoneNumber[:3] + "+" + tk.Data.Profile.Shipping.PhoneNumber[3:6] + "+" + tk.Data.Profile.Shipping.PhoneNumber[6:]
 }
 
 func (tk *Task) AwaitMonitor() {
 	pubsub, err := frame.SubscribeToChannel(tk.Data.Channels.MonitorChannel)
-	if err != nil{
+	if err != nil {
 		tk.Stop()
 		return
 	}
 	defer pubsub.Close()
 
 	tk.SetStatus(module.STATUS_MONITORING)
-	for data := range pubsub.Chan(tk.Ctx){
-		if (tk.Data.TaskData.RandomSize || existInSlice(data["size"].(string), tk.Data.TaskData.Size)) && (tk.Data.TaskData.RandomColor|| existInSlice(data["color"].(string), tk.Data.TaskData.Color)){
+	for data := range pubsub.Chan(tk.Ctx) {
+		if containsI(tk.Data.Metadata[*config.Module.Fields[3].FieldKey], data["size"].(string)) && containsI(tk.Data.Metadata[*config.Module.Fields[2].FieldKey], data["color"].(string)){
 			tk.sizeId = data["sizeId"].(string)
 			tk.PID = data["pid"].(string)
 			break
@@ -28,11 +30,10 @@ func (tk *Task) AwaitMonitor() {
 	tk.SetStatus(module.STATUS_PRODUCT_FOUND)
 }
 
-func existInSlice(keyword string, stringSlice []string)bool{
-	for _, substr := range stringSlice{
-		if substr == keyword{
-			return true
-		}
-	}
-	return false
+
+func containsI(a string, b string) bool {
+	return strings.Contains(
+		strings.ToLower(a),
+		strings.ToLower(b),
+	)
 }
